d4cbba474c2e501d8effb61bbe96fcec3dbea471
Martin Joukl <jouklma1@gmail.com>
2024-02-20 15:54:34 +0100

    Impl rest of task 14

--- src/main/java/com/martin/joukl/cv02martinjoukl/Controller/HelloController.java
+++ src/main/java/com/martin/joukl/cv02martinjoukl/Controller/HelloController.java
@@ -1,12 +1,16 @@
 package com.martin.joukl.cv02martinjoukl.Controller;
 
-import jakarta.websocket.server.PathParam;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 // @Controller slouží pro označení kontroleru pro MVC - a jeho účelem je vrátit view (tedy generovanou HTML stránku)
 // @RestController je jeho podmožina a slouží pouze pro REST odpovědi - tedy vrací data ve formě API odpovědi - JSON a nebo XML
 // RestController anotace přidává anotaci @Controller a @ResponseBody
+
+// Json - hodnoty jsou uloženy ve formátu zápisu objektu v Javascriptu - tedy ve formátu klíč - hodnota
+// Json je rozšířený jako formát request/response u REST API
+// XML - Jedná se o formát značkovacího jazyka, syntaxe je pomocí XML tagů, nevýhoda je velký overhead v těchto tagách
+// XMLko také slouží pro výměnu informací mezi servery
+// YAML - Syntaxe je poziční, jedná se primárně o konfigurační jazyk
 @RestController
 public class HelloController {
     @GetMapping("")
