a489a111b348cb35036a32a0f535f8e8f79b8df9
st64135 <martin.joukl@student.upce.cz>
2024-02-20 14:18:28 +0100

    Initial commit

---
+++ src/main/java/com/martin/joukl/cv02martinjoukl/Cv02MartinJouklApplication.java
@@ -0,0 +1,13 @@
+package com.martin.joukl.cv02martinjoukl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cv02MartinJouklApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cv02MartinJouklApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/martin/joukl/cv02martinjoukl/Cv02MartinJouklApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martin.joukl.cv02martinjoukl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cv02MartinJouklApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
